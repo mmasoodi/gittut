e63457c911b917a33d7a543c25a05c82626fca11
mmasoodi <mmasoodi@stevens.edu>
2018-09-27 10:15:55 -0400

    actually added a file

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+'''
+Created on Aug 29, 2018
+Pledge I pledge that I have abided by the Stevens honor system
+
+@author: milad
+'''
+print("Hello World")
+
+def double (x):
+    """ Thisis a function that multiply a number by two and return it"""
+    return 2 * x
+print(double(2))
